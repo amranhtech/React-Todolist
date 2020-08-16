65cef00f8dd16ddc6e25ac78bc17ba2004fd742a
amranhtech <md.amranht@gmail.com>
2020-08-16 15:08:35 +0600

    Updates

---
+++ precache-manifest.dac47424b1fceec47e0857af2feb3f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ebff8b14a99840420d82a8beeb7f311",
+    "url": "/React-Todolist/index.html"
+  },
+  {
+    "revision": "ae8d1152cdd3ad1dca18",
+    "url": "/React-Todolist/static/css/main.8dccce26.chunk.css"
+  },
+  {
+    "revision": "f42752d4855c9ce5f51c",
+    "url": "/React-Todolist/static/js/2.2735a218.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todolist/static/js/2.2735a218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8d1152cdd3ad1dca18",
+    "url": "/React-Todolist/static/js/main.54f2e406.chunk.js"
+  },
+  {
+    "revision": "77eb46d624382a02eb99",
+    "url": "/React-Todolist/static/js/runtime-main.5c9f8b40.js"
+  }
+]);
